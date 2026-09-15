a5bb2c6c582ecef16feee783f98b5581fb5641e7
Eduardo Fellipe <143148147+Eduardofellipe@users.noreply.github.com>
2026-09-15 19:45:19 -0300

    Add age verification for majority and emancipation

---
+++ python/02-condicionais/maioridade-emancipacao.py
@@ -0,0 +1,9 @@
+idade = int (input ("qual a sua idade?"))
+if idade >=18:
+    print("Você é maior de idade")
+else:
+    emancipado = input("Você é emancipado?")
+    if emancipado == "sim":
+       print ("você é maior de idade")
+    else:
+     print ("você é menor de idade")

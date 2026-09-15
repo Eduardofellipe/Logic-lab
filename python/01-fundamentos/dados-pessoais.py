ae8a570d5f8b92dd8fb30d9fd19c2bf2ff0755e6
Eduardo Fellipe <143148147+Eduardofellipe@users.noreply.github.com>
2026-09-15 19:43:38 -0300

    Create dados-pessoais.py

---
+++ python/01-fundamentos/dados-pessoais.py
@@ -0,0 +1,7 @@
+nome = input("Qual é o seu nome? ")
+idade = int(input("Quantos anos você tem? "))
+altura = float(input("Qual é a sua altura? "))
+
+print("Olá, ", nome)
+print("sua altura é: ", altura)
+print("e", idade,"anos de idade")
